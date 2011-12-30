0bae2d514fa55f0c60c0d4ccca35ec1de55f7af3
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-12-30 17:41:15 +0000

    archives are local now

--- Install-Scripts/make-archives.bash
+++ Install-Scripts/make-archives.bash
@@ -1,4 +1,6 @@
 #! /bin/bash -v
 cd ~
-sudo tar cvf - /usr/local | bzip2 -9c > usr-local.tar.bz2
-sudo tar cvf - .bashrc | bzip2 -9c > bashrc.tar.bz2
+#sudo tar cvf - /usr/local | bzip2 -9c > usr-local.tar.bz2
+tar cvf - .bashrc | bzip2 -9c > bashrc.tar.bz2
+tar cvf - perl5 | bzip2 -9c > perl5.tar.bz2
+tar cvf - R | bzip2 -9c > R.tar.bz2
