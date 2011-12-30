5215ec016736912b21b8e7cf96d9744f6e3efdf8
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-12-29 21:03:03 -0800

    stripping out the stuff we don't need any more

--- Install-Scripts/install-rstudio-desktop.bash
+++
@@ -1,4 +0,0 @@
-#! /bin/bash -v
-
-export PATH=/usr/local/bin:$PATH
-make install 2>&1 | tee ../make-install-desktop.log
--- Install-Scripts/start-webserver.bash
+++
@@ -1,7 +0,0 @@
-#! /bin/bash -v
-
-export PATH=/usr/local/sbin:/usr/local/bin:$PATH
-set|grep ^PATH
-/sbin/chkconfig rstudio-server on
-/etc/init.d/rstudio-server start
-ps -elf|grep rstudio|grep -v grep
