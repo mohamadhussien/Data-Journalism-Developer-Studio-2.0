86954ad904aeba164cf65efd7a808363f9b5af77
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-12-30 21:06:27 +0000

    dropped /usr/local

--- Install-Scripts/make-archives.bash
+++ Install-Scripts/make-archives.bash
@@ -1,6 +1,5 @@
 #! /bin/bash -v
 cd ~
-#sudo tar cvf - /usr/local | bzip2 -9c > usr-local.tar.bz2
 tar cvf - .bashrc | bzip2 -9c > bashrc.tar.bz2
 tar cvf - perl5 | bzip2 -9c > perl5.tar.bz2
 tar cvf - R | bzip2 -9c > R.tar.bz2
