dfbc16859fd2008e02a14e7c57e5069a81b2aacf
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-12-30 18:33:43 +0000

    add updates to the master scripts

--- Install-Scripts/common/install-system.bash
+++ Install-Scripts/common/install-system.bash
@@ -1,4 +1,5 @@
 #! /bin/bash -v
 
 unset JAVA_HOME # needed for rJava
+pushd ../common/; sudo ./update-libraries.bash; popd
 R --vanilla --slave < load-system.R 2>&1 | tee load-system.log
--- Install-Scripts/common/install-task-view.bash
+++ Install-Scripts/common/install-task-view.bash
@@ -1,5 +1,5 @@
 #! /bin/bash -v
 
 unset JAVA_HOME; R CMD javareconf -e # needed for rJava
-
+pushd ../common/; sudo ./update-libraries.bash; popd
 R --vanilla --slave < load-task-view.R 2>&1 | tee load-task-view.log
