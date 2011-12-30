bfa1d04a585e30f22671758387d782be8b831c62
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-12-30 18:46:37 +0000

    added perl-Crypt-SSLeay

--- Install-Scripts/Perl-packages/install-dependencies.bash
+++ Install-Scripts/Perl-packages/install-dependencies.bash
@@ -1,4 +1,5 @@
 #! /bin/bash -v
 
-sudo zypper install -y perl-YAML perl-libwww-perl perl-Sub-Identify
+sudo zypper install -y perl-YAML perl-libwww-perl perl-Sub-Identify \
+  perl-Crypt-SSLeay
 wget http://search.cpan.org/CPAN/authors/id/A/AP/APEIRON/local-lib-1.008004.tar.gz
