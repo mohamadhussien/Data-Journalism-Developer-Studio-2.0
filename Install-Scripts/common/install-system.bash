c4cb261415e3ac1057e51eb9ab869e77b6f3fb37
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-12-30 21:09:32 -0800

    update both system and local libraries!

--- Install-Scripts/common/install-system.bash
+++ Install-Scripts/common/install-system.bash
@@ -2,4 +2,5 @@
 
 unset JAVA_HOME # needed for rJava
 pushd ../common/; sudo ./update-libraries.bash; popd
+pushd ../common/; ./update-libraries.bash; popd
 R --vanilla --slave < load-system.R 2>&1 | tee load-system.log
--- Install-Scripts/common/install-task-view.bash
+++ Install-Scripts/common/install-task-view.bash
@@ -2,4 +2,5 @@
 
 unset JAVA_HOME; R CMD javareconf -e # needed for rJava
 pushd ../common/; sudo ./update-libraries.bash; popd
+pushd ../common/; ./update-libraries.bash; popd
 R --vanilla --slave < load-task-view.R 2>&1 | tee load-task-view.log
