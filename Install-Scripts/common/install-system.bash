8f8b59e62a0b342662ff528414ce484123e15180
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-12-31 09:10:03 -0800

    global update.libraries only

--- Install-Scripts/common/install-system.bash
+++ Install-Scripts/common/install-system.bash
@@ -2,5 +2,4 @@
 
 unset JAVA_HOME # needed for rJava
 pushd ../common/; sudo ./update-libraries.bash; popd
-pushd ../common/; ./update-libraries.bash; popd
 R --vanilla --slave < load-system.R 2>&1 | tee load-system.log
--- Install-Scripts/common/install-task-view.bash
+++ Install-Scripts/common/install-task-view.bash
@@ -2,5 +2,4 @@
 
 unset JAVA_HOME # needed for rJava
 pushd ../common/; sudo ./update-libraries.bash; popd
-pushd ../common/; ./update-libraries.bash; popd
 R --vanilla --slave < load-task-view.R 2>&1 | tee load-task-view.log
