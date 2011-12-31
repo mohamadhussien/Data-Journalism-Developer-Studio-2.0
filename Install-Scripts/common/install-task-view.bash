0328acb90107653b9463e79cb3d199676aa473d9
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-12-31 09:07:02 -0800

    removed javareconf

--- Install-Scripts/common/install-task-view.bash
+++ Install-Scripts/common/install-task-view.bash
@@ -1,6 +1,6 @@
 #! /bin/bash -v
 
-unset JAVA_HOME; R CMD javareconf -e # needed for rJava
+unset JAVA_HOME # needed for rJava
 pushd ../common/; sudo ./update-libraries.bash; popd
 pushd ../common/; ./update-libraries.bash; popd
 R --vanilla --slave < load-task-view.R 2>&1 | tee load-task-view.log
