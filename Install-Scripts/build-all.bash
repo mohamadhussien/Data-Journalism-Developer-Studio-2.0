cbf58ae0e3f93ac352707b51a25fe2b4fd818957
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-12-30 23:29:09 -0800

    account for Perl-packages, R-libraries and Rcmdr in initial build

--- Install-Scripts/build-all.bash
+++ Install-Scripts/build-all.bash
@@ -6,19 +6,22 @@ do
   j=`echo ${i} | sed 's/cleanup.bash//'`
   pushd ${j}
   ./cleanup.bash
-  ./install-dependencies.bash
+  ./install-dependencies.bash 2>&1 | tee dependencies.log
   popd
 done
 
-# Perl packages have to be done first for a few other things to work
-pushd Perl-packages
-./install-all.bash
-popd
-
 for i in */cleanup.bash
 do
-  if [ "${i}" != "Perl-packages/cleanup.bash" ]
+  if [ "${i}" == "Perl-packages/cleanup.bash" ]
   then
+    echo "skipped"
+  elif [ "${i}" == "R-libraries/cleanup.bash" ]
+  then
+    echo "skipped"
+  elif [ "${i}" == "Rcmdr/cleanup.bash" ]
+  then
+    echo "skipped"
+  else
     j=`echo ${i} | sed 's/cleanup.bash//'`
     pushd ${j}
     ./install-all.bash
