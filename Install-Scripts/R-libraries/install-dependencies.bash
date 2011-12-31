1ce7cbf79c6a695eef9b2dbd7e6ec68105fae94a
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-12-30 21:01:04 -0800

    moved dependencies to logical places

--- Install-Scripts/R-libraries/install-dependencies.bash
+++ Install-Scripts/R-libraries/install-dependencies.bash
@@ -1,3 +1,11 @@
 #! /bin/bash -v
 
-sudo zypper install -y tk-devel
+# for building packages
+sudo zypper install -y make gcc gcc-c++ gcc-fortran java-1_6_0-openjdk-devel
+
+# needed for LaTeX / SWeave
+sudo zypper install -y texlive-bin-latex texlive-bin libxml2-devel texinfo
+
+# needed for R compile from source and packages
+sudo zypper install -y freeglut-devel libpng14-devel libpng14-compat-devel \
+  tk-devel
--- Install-Scripts/install-dependencies.bash
+++ Install-Scripts/install-dependencies.bash
@@ -1,18 +1,5 @@
 #!/bin/bash -v
 
-#
-# install-dependencies.bash
-#
-# Copyright (C) 2009-11 by M. Edward (Ed) Borasky
-#
-# This program is licensed to you under the terms of version 3 of the
-# GNU Affero General Public License. This program is distributed WITHOUT
-# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
-# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
-# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
-#
-#
-
 # wget and git may not be there in appliances
 sudo zypper install -y wget git-core
 
@@ -22,15 +9,9 @@ sudo zypper install -y vim
 # command line conveniences
 sudo zypper install -y command-not-found findutils-locate lynx
 
-# needed for R tcl/tk builds
-sudo zypper install -y tcl-devel tk-devel
-
-# needed for LaTeX / SWeave
-sudo zypper install -y texlive-bin-latex texlive-bin libxml2-devel texinfo
+# system admin
+sudo zypper install -y sudo ntp yast2-sudo yast2-ntp-client
 
 # Just in case there's no browser
-sudo zypper install -y MozillaFirefox MozillaFirefox-branding-openSUSE \
-  icedtea-web
-
-# needed for R compile from source and packages
-sudo zypper install -y freeglut-devel libpng14-devel
+sudo zypper install -y MozillaFirefox icedtea-web chromium-desktop-gnome \
+  vpx-tools
