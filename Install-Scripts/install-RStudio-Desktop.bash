06d52f6bcc2aba4caeb5b66915f309d663008dfd
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-12-31 16:54:38 -0800

    ugly hack to unset JAVA_HOME when starting rstudio :-(

--- Install-Scripts/install-RStudio-Desktop.bash
+++ Install-Scripts/install-RStudio-Desktop.bash
@@ -6,3 +6,12 @@ then
 else
   sudo rpm -Uvh http://download1.rstudio.org/rstudio-0.94.110-i686.rpm
 fi
+
+# hack rstudio executable to unset JAVA_HOME
+if [ ! -e "/usr/lib/rstudio/bin/rstudio.exe" ]
+then
+  pushd /usr/lib/rstudio/bin
+  sudo mv rstudio rstudio.exe
+  popd
+  sudo cp rstudio /usr/lib/rstudio/bin
+fi
