1c1350372910afd607a6db2c01525dfcaad832a2
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-01 20:57:47 -0800

    run R CMD javareconf, *not* unset JAVA_HOME!

--- Install-Scripts/common/install-task-view.bash
+++ Install-Scripts/common/install-task-view.bash
@@ -1,5 +1,5 @@
 #! /bin/bash -v
 
-unset JAVA_HOME # needed for rJava
+sudo R CMD javareconf
 pushd ../common/; sudo ./update-libraries.bash; popd
 R --vanilla --slave < load-task-view.R 2>&1 | tee load-task-view.log
