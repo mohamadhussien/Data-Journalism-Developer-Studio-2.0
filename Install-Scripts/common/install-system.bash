59f3c593088b99606ceee2e08ec44d18b686db32
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-01 21:04:17 -0800

    should not need more than *one* javareconf!

--- Install-Scripts/common/install-system.bash
+++ Install-Scripts/common/install-system.bash
@@ -1,5 +1,4 @@
 #! /bin/bash -v
 
-sudo R CMD javareconf
 pushd ../common/; sudo ./update-libraries.bash; popd
 R --vanilla --slave < load-system.R 2>&1 | tee load-system.log
--- Install-Scripts/common/install-task-view.bash
+++ Install-Scripts/common/install-task-view.bash
@@ -1,5 +1,4 @@
 #! /bin/bash -v
 
-sudo R CMD javareconf
 pushd ../common/; sudo ./update-libraries.bash; popd
 R --vanilla --slave < load-task-view.R 2>&1 | tee load-task-view.log
