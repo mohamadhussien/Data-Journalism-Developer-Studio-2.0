1817970a3afaed021fb51f7a9d6fb77886e01caa
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-02 23:20:05 -0800

    removed Chromium and Lynx

--- Install-Scripts/install-dependencies.bash
+++ Install-Scripts/install-dependencies.bash
@@ -11,14 +11,13 @@ sudo zypper install -y wget git-core
 sudo zypper install -y vim
 
 # command line conveniences
-sudo zypper install -y command-not-found findutils-locate lynx
+sudo zypper install -y command-not-found findutils-locate
 
 # system admin
 sudo zypper install -y sudo ntp yast2-sudo yast2-ntp-client
 
 # Just in case there's no browser
-sudo zypper install -y MozillaFirefox icedtea-web chromium-desktop-gnome \
-  vpx-tools
+sudo zypper install -y MozillaFirefox icedtea-web
 
 # desktop goodies
 sudo zypper install -y nautilus-evince nautilus-open-terminal libgnome
