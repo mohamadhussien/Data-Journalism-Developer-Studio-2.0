e724be49f1b4c7035e4d05ce4abd25310f21d3fd
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-05 12:26:11 -0800

    cleanup and test - glitchy behavior

--- Install-Scripts/Perl-packages/install-dependencies.bash
+++ Install-Scripts/Perl-packages/install-dependencies.bash
@@ -2,4 +2,3 @@
 
 sudo zypper install -y perl-YAML perl-Crypt-SSLeay perl-YAML-Syck \
   perl-IO-Socket-SSL perl-Net-SSLeay perl-JSON-XS perl-JSON
-wget http://search.cpan.org/CPAN/authors/id/A/AP/APEIRON/local-lib-1.008004.tar.gz
--- Install-Scripts/Perl-packages/install-local-lib.bash
+++ Install-Scripts/Perl-packages/install-local-lib.bash
@@ -1,6 +1,9 @@
 #! /bin/bash -v
 
 rm -fr ~/.cpan ~/perl5 # clean slate
+vim ~/.bashrc
+wget \
+  http://search.cpan.org/CPAN/authors/id/A/AP/APEIRON/local-lib-1.008004.tar.gz
 tar xf local-lib*gz
 pushd local-lib*
 perl Makefile.PL --bootstrap
@@ -9,10 +12,9 @@ make test
 make install
 echo 'eval $(perl -I${HOME}/perl5/lib/perl5 -Mlocal::lib)' >> ~/.bashrc
 echo 'export MANPATH=${MANPATH}:${HOME}/perl5/man' >>~/.bashrc
+vim ~/.bashrc
 popd
 
-# hack the configuration so installs work!
+# configure / update cpan
+source ~/.bashrc
 cpan < prep1.cpan
-cp ~/.cpan/CPAN/MyConfig.pm .
-sed "s:/root/:/${HOME}/:" MyConfig.pm > ~/.cpan/CPAN/MyConfig.pm
-rm MyConfig.pm
