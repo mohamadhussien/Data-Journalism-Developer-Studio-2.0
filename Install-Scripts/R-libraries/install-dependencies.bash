d14e5a4ad3819ca0917a606aa4092b0287cdcc8e
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-04 19:58:52 -0800

    removed libpng14-compat-devel

--- Install-Scripts/R-libraries/install-dependencies.bash
+++ Install-Scripts/R-libraries/install-dependencies.bash
@@ -7,5 +7,4 @@ sudo zypper install -y make gcc gcc-c++ gcc-fortran java-1_6_0-openjdk-devel
 sudo zypper install -y texlive-bin-latex texlive-bin libxml2-devel texinfo
 
 # needed for R compile from source and packages
-sudo zypper install -y freeglut-devel libpng14-devel libpng14-compat-devel \
-  tk-devel
+sudo zypper install -y freeglut-devel libpng14-devel tk-devel
