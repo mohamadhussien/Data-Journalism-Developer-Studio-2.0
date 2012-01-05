879c7dc7f1fa4fe3c7779bbdaf70d9ff1dfb869f
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-05 14:32:58 -0800

    call me old-fashioned -- added SeaMonkey ;-)

--- Install-Scripts/BrowserDevelopment/install-dependencies.bash
+++ Install-Scripts/BrowserDevelopment/install-dependencies.bash
@@ -3,3 +3,4 @@
 sudo zypper install -y chromium-desktop-gnome vpx-tools
 sudo zypper install -y v8-devel libvpx-devel
 sudo zypper install -y MozillaFirefox-devel
+sudo zypper install -y seamonkey seamonkey-dom-inspector seamonkey-venkman
