4355fd509c1dbd05763c8b123a80fefd9bf8aa59
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-05 21:35:15 -0800

    install Node.js from Factory repo instead of source

--- Install-Scripts/Node/install-dependencies.bash
+++ Install-Scripts/Node/install-dependencies.bash
@@ -1,1 +1,7 @@
 #! /bin/bash -v
+
+sudo zypper addrepo -cf \
+  http://download.opensuse.org/repositories/devel:/languages:/nodejs/openSUSE_Factory/devel:languages:nodejs.repo
+sudo zypper refresh
+
+sudo zypper install -y nodejs nodejs-devel
