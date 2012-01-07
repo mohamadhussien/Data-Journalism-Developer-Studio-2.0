684771412f1ede90a75711e24894c424cf6f67ed
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-06 22:28:54 -0800

    ditched paper on Social Network Analysis and command line conveniences

--- Install-Scripts/install-dependencies.bash
+++ Install-Scripts/install-dependencies.bash
@@ -5,14 +5,11 @@ sudo zypper addrepo -cf \
   http://download.opensuse.org/repositories/openSUSE:/Factory:/Contrib/openSUSE_12.1/openSUSE:Factory:Contrib.repo
 sudo zypper refresh
 
-# wget and git may not be there in appliances
+# wget may not be there in appliances
 sudo zypper install -y wget
 
 # vim
 sudo zypper install -y vim
-
-# command line conveniences
-sudo zypper install -y command-not-found findutils-locate bash-completion
 
 # system admin
 sudo zypper install -y sudo ntp yast2-sudo yast2-ntp-client
