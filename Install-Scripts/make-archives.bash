214df5633f0120dc67f697da40911560ff62485a
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-07 21:44:14 -0800

    no Maqetta desktop file now

--- Install-Scripts/make-archives.bash
+++ Install-Scripts/make-archives.bash
@@ -6,6 +6,5 @@ tar cvf - perl5 | bzip2 -9c > perl5.tar.bz2
 tar cvf - R | bzip2 -9c > R.tar.bz2
 tar cvf - \
   local \
-  .local/share/applications/maqetta.desktop \
   .local/share/applications/google-refine.desktop \
   | bzip2 -9c > local.tar.bz2
