c7c891c6f6f7beefd8a3ace01c65b23bdcec0687
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-09 20:54:48 -0800

    buh bye beancounter

--- Install-Scripts/Finance/install-all.bash
+++ Install-Scripts/Finance/install-all.bash
@@ -1,7 +1,7 @@
 #! /bin/bash -v
 
 # warning: quantlib takes a *long* time and a lot of space
-for i in beancounter quantlib armadillo task-view
+for i in quantlib armadillo task-view
 do
   ./install-${i}.bash 2>&1 | tee ${i}.log
 done
