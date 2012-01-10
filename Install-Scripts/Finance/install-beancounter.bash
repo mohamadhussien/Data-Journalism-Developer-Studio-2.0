dd407ac7c3119c35b977f5b3917279c7f2047587
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-09 20:46:23 -0800

    beancounter is failing tests - deleted

--- Install-Scripts/Finance/install-beancounter.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -v
-
-source ~/.bashrc
-cpan Finance::BeanCounter
-
-# edit the setup script
-diff setup_beancounter ~/perl5/bin/setup_beancounter # our version
-sudo cp setup_beancounter ~/perl5/bin/setup_beancounter # our version
