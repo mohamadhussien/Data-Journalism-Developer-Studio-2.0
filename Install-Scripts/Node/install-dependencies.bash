dd88779f8f0cbdea8b4a28e9703d1913feadb868
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-12 10:20:06 -0800

    switch to Tumbleweed repo

--- Install-Scripts/Node/install-dependencies.bash
+++ Install-Scripts/Node/install-dependencies.bash
@@ -1,6 +1,6 @@
 #! /bin/bash -v
 
-sudo zypper addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/nodejs/openSUSE_Factory/devel:languages:nodejs.repo
+sudo zypper addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/nodejs/openSUSE_Tumbleweed/devel:languages:nodejs.repo
 sudo zypper refresh
 
 sudo zypper install -y nodejs nodejs-devel
