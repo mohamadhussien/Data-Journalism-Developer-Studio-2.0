a94057dcfc05438d2ea172085cb0780fffc94adf
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-23 15:05:04 -0800

    added epdfview

--- Install-Scripts/install-dependencies.bash
+++ Install-Scripts/install-dependencies.bash
@@ -1,4 +1,4 @@
 #!/bin/bash -v
 
 sudo zypper install -y bash-completion sudo ntp MozillaFirefox icedtea-web \
-  ruby wget yast2-sudo yast2-ntp-client yast2-firewall
+  ruby wget yast2-sudo yast2-ntp-client yast2-firewall epdfview
