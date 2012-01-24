d44418faa054c8e5a0b754973116d31b587ec777
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-23 23:53:47 -0800

    refactoring - IceWM base

--- Install-Scripts/install-dependencies.bash
+++ Install-Scripts/install-dependencies.bash
@@ -1,4 +1,11 @@
 #!/bin/bash -v
 
+# IceWM base
+sudo zypper install -y alacarte bash-completion iceWMCP iceWMCP-addons \
+  icewm-themes lightdm NetworkManager ntp obconf openbox open-vm-tools \
+  open-vm-tools-gui sudo virtualbox-guest-tools virtualbox-guest-x11 \
+  xscreensaver yast2-firewall yast2-ntp-client yast2-sudo
+
+# IceWM base studio
 sudo zypper install -y bash-completion sudo ntp MozillaFirefox icedtea-web \
   ruby wget yast2-sudo yast2-ntp-client yast2-firewall epdfview
