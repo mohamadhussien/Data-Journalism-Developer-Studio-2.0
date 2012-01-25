3a83420513f75cbdf0a80670d8ddcf3ec8167000
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-25 12:35:49 -0800

    Openbox menu editing added

--- Install-Scripts/make-archives.bash
+++ Install-Scripts/make-archives.bash
@@ -1,6 +1,9 @@
 #! /bin/bash -v
 cd ~
-gmenu-simple-editor # invoke menu editor
+gmenu-simple-editor # invoke "Gnome" menu editor
+mkdir -p ~/.config/openbox
+cp /etc/xdg/openbox/*.xml ~/.config/openbox/
+obmenu # invoke Openbox menu editor
 vim .bashrc # get approval before archiving .bashrc!
 tar cvf - .bashrc | bzip2 -9c > bashrc.tar.bz2
 tar cvf - perl5 | bzip2 -9c > perl5.tar.bz2
@@ -9,4 +12,5 @@ tar cvf - \
   local \
   .local/share/applications/google-refine.desktop \
   .local/share/applications/R.desktop \
+  .config/openbox \
   | bzip2 -9c > local.tar.bz2
