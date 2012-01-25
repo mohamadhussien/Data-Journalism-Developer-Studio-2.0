c4861730dc28cfd3ff1ae19fe9709c126c934999
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-25 13:23:34 -0800

    removed unused packages!!!!!

--- Install-Scripts/Perl-packages/install-packages.bash
+++ Install-Scripts/Perl-packages/install-packages.bash
@@ -5,27 +5,10 @@ source ~/.bashrc
 for i in \
   Sys::Statistics::Linux \
   AnyEvent::Twitter::Stream \
-  AnyEvent::IRC \
-  AnyEvent::IRC::Connection \
-  AnyEvent::IRC::Client \
-  AnyEvent::IRC::Util \
   Browser::Open \
-  DBD::SQLite \
-  Facebook \
   Hash::Flatten \
   Net::Twitter \
-  Statistics::R \
-  WWW::Mechanize \
-  Data::Dumper \
-  Email::Send::Gmail \
-  Email::Send \
-  Email::Sender::Simple \
-  Email::Simple::Creator \
-  Email::Simple \
-  HTTP::Client::Parallel \
-  Net::Google::Spreadsheets \
-  WebService::Bitly \
-  Google::Voice
+  WWW::Mechanize
 do
   echo "install $i" | cpan 2>&1 | tee $i.log
 done
