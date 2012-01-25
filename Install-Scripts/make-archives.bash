cace92355e9a6435442401828c585285c221d72e
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-25 12:29:22 -0800

    new menu editor

--- Install-Scripts/make-archives.bash
+++ Install-Scripts/make-archives.bash
@@ -1,6 +1,6 @@
 #! /bin/bash -v
 cd ~
-alacarte # invoke menu editor
+gmenu-simple-editor # invoke menu editor
 vim .bashrc # get approval before archiving .bashrc!
 tar cvf - .bashrc | bzip2 -9c > bashrc.tar.bz2
 tar cvf - perl5 | bzip2 -9c > perl5.tar.bz2
