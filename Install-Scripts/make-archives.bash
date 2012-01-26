77a136b2f3e9d98f30d6995e67e0d31a0cbc4378
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-26 13:58:27 -0800

    added archive of /usr/local for Sigil

--- Install-Scripts/make-archives.bash
+++ Install-Scripts/make-archives.bash
@@ -7,6 +7,7 @@ vim .bashrc # get approval before archiving .bashrc!
 tar cvf - .bashrc | bzip2 -9c > bashrc.tar.bz2
 tar cvf - perl5 | bzip2 -9c > perl5.tar.bz2
 tar cvf - R | bzip2 -9c > R.tar.bz2
+sudo tar cvf - /usr/local | bzip2 -9c > usr-local.tar.bz2
 tar cvf - \
   local \
   .local/share/applications/google-refine.desktop \
