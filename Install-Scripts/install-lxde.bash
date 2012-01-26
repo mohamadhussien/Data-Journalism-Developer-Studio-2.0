6144061f0577283c0eaaababe36a1375bc507f9b
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-25 22:02:29 -0800

    don't need a script to install LXDE or GNOME or KDE - YaST2 is your friend!

--- Install-Scripts/install-lxde.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -v
-
-sudo zypper install -y -t pattern lxde
-sudo zypper install -y lxappearance lxappearance-obconf lxcc lxde-common \
-  lxinput lxlauncher lxmenu-data lxmusic lxpanel lxpolkit lxrandr lxsession \
-  lxsession-edit lxshortcut lxtask lxterminal menu-cache nuoveXT2-icon-theme \
-  parcellite pcmanfm
