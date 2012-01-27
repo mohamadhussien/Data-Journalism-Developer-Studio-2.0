5b8af8be5bcda0a638d4c12cc56a87cd1de1c9c5
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-27 12:05:45 -0800

    put all the magic back - not enough space saved to be worth it

--- Install-Scripts/install-razor-qt.bash
+++ Install-Scripts/install-razor-qt.bash
@@ -2,4 +2,6 @@
 
 sudo zypper addrepo -cf http://download.opensuse.org/repositories/X11:/QtDesktop/openSUSE_12.1/X11:QtDesktop.repo
 sudo zypper refresh
-sudo zypper install -y qlipper qterminal razorqt rssguard
+sudo zypper install -y juffed juffed-plugins qlipper qstardict qterminal \
+	  qxkb razorqt rssguard
+
