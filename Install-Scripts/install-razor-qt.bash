b5491ac5487037e26ffd50ccbdffdf4eabca92fc
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-27 11:56:03 -0800

    dropped dictionary and keyboard mapper

--- Install-Scripts/install-razor-qt.bash
+++ Install-Scripts/install-razor-qt.bash
@@ -2,4 +2,4 @@
 
 sudo zypper addrepo -cf http://download.opensuse.org/repositories/X11:/QtDesktop/openSUSE_12.1/X11:QtDesktop.repo
 sudo zypper refresh
-sudo zypper install -y qlipper qstardict qterminal qxkb razorqt rssguard
+sudo zypper install -y qlipper qterminal razorqt rssguard
