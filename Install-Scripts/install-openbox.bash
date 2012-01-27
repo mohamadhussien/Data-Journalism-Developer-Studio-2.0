316599b9ea13c602c9129693b2924efe6efa24ad
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-26 22:15:28 -0800

    dropped obmenu - scripts aren't using it

--- Install-Scripts/install-openbox.bash
+++ Install-Scripts/install-openbox.bash
@@ -1,7 +1,4 @@
 #!/bin/bash -v
 
-# Repositories
-sudo zypper addrepo -cf http://download.opensuse.org/repositories/X11:/lxde/openSUSE_12.1/X11:lxde.repo # obmenu is here
-
 # Openbox
-sudo zypper install -y obconf obmenu openbox
+sudo zypper install -y obconf openbox
