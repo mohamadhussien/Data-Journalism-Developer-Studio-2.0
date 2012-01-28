8a5689ff2a8562e5d305ae96114890f3e316cacb
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-27 21:45:31 -0800

    added browser desktop files

--- Install-Scripts/make-archives.bash
+++ Install-Scripts/make-archives.bash
@@ -8,9 +8,11 @@ sudo tar cvf - /usr/local | bzip2 -9c > usr-local.tar.bz2
 tar cvf - \
   local \
   .local/share/applications/google-refine.desktop \
+  .local/share/applications/google-refine-browser.desktop \
+  .local/share/applications/maqetta.desktop \
+  .local/share/applications/maqetta-browser.desktop \
   .local/share/applications/R.desktop \
-  .razor/* \
-  .config/openbox/menu.xml \
+  .config/openbox* \
   .vimrc \
   .gvimrc \
   | bzip2 -9c > local.tar.bz2
