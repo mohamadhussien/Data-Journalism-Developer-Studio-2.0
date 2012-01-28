6f6f5b7a5b5d001dc369627898b25538f6bda8af
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-28 01:57:48 -0800

    no more Razor

--- Install-Scripts/install-repositories.bash
+++ Install-Scripts/install-repositories.bash
@@ -4,5 +4,4 @@ sudo zypper addrepo -cf http://download.opensuse.org/repositories/Virtualization
 sudo zypper addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/perl/openSUSE_12.1/devel:languages:perl.repo # Perl libraries
 sudo zypper addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/R:/patched/openSUSE_12.1/devel:languages:R:patched.repo # R Patched
 sudo zypper addrepo -cf http://download.opensuse.org/repositories/Documentation:/Tools/openSUSE_12.1/Documentation:Tools.repo # Tesseract
-sudo zypper addrepo -cf http://download.opensuse.org/repositories/X11:/QtDesktop/openSUSE_12.1/X11:QtDesktop.repo # Razor Qt
 sudo zypper update
