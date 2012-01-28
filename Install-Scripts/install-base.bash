0b23a210b69d3e4ccab5ee3b997c35b2516d7587
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-28 01:54:50 -0800

    split out desktop configurations

--- Install-Scripts/install-base.bash
+++ Install-Scripts/install-base.bash
@@ -6,15 +6,6 @@ then
   sudo zypper -n rr znmeb_openSUSE_12.1
 fi
 
-cat bashrc >> ~/.bashrc # define the ls conveniences
-vim ~/.bashrc
-source ~/.bashrc
-
-cp vimrc ~/.vimrc
-cp gvimrc ~/.gvimrc
-mkdir -p ~/.config/openbox; cp -a openbox-conf/* ~/.config/openbox/
-mkdir -p ~/.config/lxterminal; cp -a lxterminal-conf/* ~/.config/lxterminal/
-
 # install Linux dependencies
 if [ "$1" != "--appliance" ]
 then
