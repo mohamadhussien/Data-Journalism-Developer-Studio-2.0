daaa4912c2f774f92c0076cd35b0017a3a2bf94f
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-27 21:42:54 -0800

    added Patterns; all packages listed in Studio

--- Install-Scripts/install-dependencies.bash
+++ Install-Scripts/install-dependencies.bash
@@ -1,5 +1,6 @@
 #!/bin/bash -v
 
 ./install-repositories.bash 2>&1 | tee repositories.log
+./install-patterns.bash 2>&1 | tee patterns.log
 ./install-packages.bash 2>&1 | tee packages.log
 ./install-rstudio.bash 2>&1 | tee rstudio.log
---
+++ Install-Scripts/install-patterns.bash
@@ -0,0 +1,5 @@
+#! /bin/bash -v
+
+sudo zypper install -y -t pattern base
+sudo zypper install -y -t pattern lxde
+sudo zypper install -y -t pattern x11
