4f2c92632ff290c2e0fd20bf4452d44fdea387b1
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-28 00:35:56 -0800

    lxterminal configuration

--- Install-Scripts/make-archives.bash
+++ Install-Scripts/make-archives.bash
@@ -12,7 +12,7 @@ tar cvf - \
   .local/share/applications/maqetta.desktop \
   .local/share/applications/maqetta-browser.desktop \
   .local/share/applications/R.desktop \
-  .config/openbox* \
+  .config/openbox/* \
   .vimrc \
   .gvimrc \
   | bzip2 -9c > local.tar.bz2
