da266510d6e6b3e44f03773655f7a0a7a7304acf
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-29 10:21:32 -0800

    make archive during install

--- Install-Scripts/GoogleRefine/install-google-refine.bash
+++ Install-Scripts/GoogleRefine/install-google-refine.bash
@@ -1,6 +1,7 @@
 #! /bin/bash -v
 export WHERE=http://google-refine.googlecode.com/files
 export WHAT=google-refine-2.5-r2407.tar.gz
+export VERSION=`echo ${WHAT}|sed 's/.tar.gz//'`
 export DIR=google-refine
 
 mkdir -p ~/local
@@ -9,6 +10,7 @@ rm -fr ${WHAT} ${DIR}
 wget ${WHERE}/${WHAT}
 tar xf ${WHAT}
 rm ${WHAT}
+mv google-refine-* ${DIR}
 popd
 
 mkdir -p ~/.local/share/applications
@@ -16,3 +18,8 @@ sed s:HOME:$HOME: google-refine.desktop > \
   ~/.local/share/applications/google-refine.desktop
 sed s:HOME:$HOME: google-refine-browser.desktop > \
   ~/.local/share/applications/google-refine-browser.desktop
+
+tar cf - \
+  ~/.local/share/applications/google-refine*desktop \
+  ~/local/${DIR} \
+  | bzip2 -9c > ~/${VERSION}.tar.bz2
