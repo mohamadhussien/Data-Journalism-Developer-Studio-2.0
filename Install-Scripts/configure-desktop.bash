67111f42135c1b9a4042b7980940ec216ebb8906
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-28 19:49:57 -0800

    don't need vimrc

--- Install-Scripts/configure-desktop.bash
+++ Install-Scripts/configure-desktop.bash
@@ -4,7 +4,6 @@ cat bashrc >> ~/.bashrc # define the ls conveniences
 vim ~/.bashrc
 source ~/.bashrc
 
-cp vimrc ~/.vimrc
 mkdir -p ~/.config/openbox; cp -a openbox-conf/* ~/.config/openbox/
 mkdir -p ~/.config/lxterminal; cp -a lxterminal-conf/* ~/.config/lxterminal/
 mkdir -p ~/.local/share/applications
