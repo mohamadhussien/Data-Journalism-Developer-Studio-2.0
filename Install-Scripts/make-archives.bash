abf61f963d716b507d8a0085babba63c4e9679e0
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-29 11:17:34 -0800

    remove Perl archive - done during install

--- Install-Scripts/make-archives.bash
+++ Install-Scripts/make-archives.bash
@@ -2,7 +2,6 @@
 cd ~
 vim .bashrc # get approval before archiving .bashrc!
 tar cvf - .bashrc | bzip2 -9c > bashrc.tar.bz2
-tar cvf - perl5 | bzip2 -9c > perl5.tar.bz2
 tar cvf - \
   .local/share/applications/R.desktop \
   R \
