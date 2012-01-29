c78b359313baee767d6c300ab7f90c52680fac34
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-01-29 10:36:30 -0800

    moved packages to individual archives

--- Install-Scripts/make-archives.bash
+++ Install-Scripts/make-archives.bash
@@ -3,15 +3,11 @@ cd ~
 vim .bashrc # get approval before archiving .bashrc!
 tar cvf - .bashrc | bzip2 -9c > bashrc.tar.bz2
 tar cvf - perl5 | bzip2 -9c > perl5.tar.bz2
-tar cvf - R | bzip2 -9c > R.tar.bz2
-sudo tar cvf - /usr/local | bzip2 -9c > usr-local.tar.bz2
 tar cvf - \
-  local \
-  .local/share/applications/google-refine.desktop \
-  .local/share/applications/google-refine-browser.desktop \
-  .local/share/applications/maqetta.desktop \
-  .local/share/applications/maqetta-browser.desktop \
   .local/share/applications/R.desktop \
+  R \
+  | bzip2 -9c > R.tar.bz2
+tar cvf - \
   .config/openbox/* \
   .config/lxterminal/* \
-  | bzip2 -9c > local.tar.bz2
+  | bzip2 -9c > config.tar.bz2
