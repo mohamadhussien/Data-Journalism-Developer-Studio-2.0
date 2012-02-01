b1ff184198b37c8a5b2a9eb04b6014c86430bb96
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-01 10:20:41 -0800

    don't need whole kernel source tree!

--- Install-Scripts/vbox-guest/install-dependencies.bash
+++ Install-Scripts/vbox-guest/install-dependencies.bash
@@ -1,8 +1,8 @@
 #! /bin/bash -v
 
 # tools to build from source
-sudo zypper refresh
-sudo zypper install make gcc kernel-source kernel-syms
+export KERNEL=`uname -r|sed 's/^.*-//'`
+sudo zypper install kernel-${KERNEL}-devel
 
 # get rid of any existing ones
 sudo zypper remove virtualbox-guest-kmp-default virtualbox-guest-kmp-desktop \
