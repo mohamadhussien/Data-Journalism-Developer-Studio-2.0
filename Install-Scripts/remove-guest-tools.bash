cd5657dc6ea99ff4eee0c403789301d29e22de03
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-02 12:36:53 -0800

    added mkinitrd and reboot

--- Install-Scripts/remove-guest-tools.bash
+++ Install-Scripts/remove-guest-tools.bash
@@ -12,4 +12,6 @@ sudo zypper remove libvmtools0 open-vm-tools open-vm-tools-gui \
 sudo zypper remove virtualbox-guest-kmp-default virtualbox-guest-kmp-desktop \
   virtualbox-guest-kmp-pae virtualbox-guest-tools virtualbox-guest-x11
 
-sudo /sbin/yast2 # fix the mkinitrd stuff by hand
+# mkinitrd and reboot
+sudo /sbin/mkinitrd
+sudo /sbin/init 6
