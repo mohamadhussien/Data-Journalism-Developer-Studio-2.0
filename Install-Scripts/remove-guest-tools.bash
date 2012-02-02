8c3f114cf22ced09b15d3143216223b2a1969188
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-02 10:39:27 -0800

    added a YaST2 step to clean up initrd

--- Install-Scripts/remove-guest-tools.bash
+++ Install-Scripts/remove-guest-tools.bash
@@ -11,3 +11,5 @@ sudo zypper remove libvmtools0 open-vm-tools open-vm-tools-gui \
 # get rid of any VirtualBox ones
 sudo zypper remove virtualbox-guest-kmp-default virtualbox-guest-kmp-desktop \
   virtualbox-guest-kmp-pae virtualbox-guest-tools virtualbox-guest-x11
+
+sudo /sbin/yast2 # fix the mkinitrd stuff by hand
