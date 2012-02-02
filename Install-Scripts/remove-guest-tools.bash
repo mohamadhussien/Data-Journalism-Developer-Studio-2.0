223d64dba73da51b8ef37118392125a0c90b813c
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-02 08:37:59 -0800

    merged VMware and VirtualBox removal

--- Install-Scripts/remove-guest-tools.bash
+++ Install-Scripts/remove-guest-tools.bash
@@ -4,6 +4,10 @@
 export KERNEL=`uname -r|sed 's/^.*-//'`
 sudo zypper install kernel-${KERNEL}-devel
 
-# get rid of any existing ones
+# get rid of any VMware tools
 sudo zypper remove libvmtools0 open-vm-tools open-vm-tools-gui \
   vmware-guest-kmp-default vmware-guest-kmp-desktop vmware-guest-kmp-pae
+
+# get rid of any VirtualBox ones
+sudo zypper remove virtualbox-guest-kmp-default virtualbox-guest-kmp-desktop \
+  virtualbox-guest-kmp-pae virtualbox-guest-tools virtualbox-guest-x11
--- Install-Scripts/vbox-guest/install-dependencies.bash
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -v
-
-# tools to build from source
-export KERNEL=`uname -r|sed 's/^.*-//'`
-sudo zypper install kernel-${KERNEL}-devel
-
-# get rid of any existing ones
-sudo zypper remove virtualbox-guest-kmp-default virtualbox-guest-kmp-desktop \
-  virtualbox-guest-kmp-pae virtualbox-guest-tools virtualbox-guest-x11
