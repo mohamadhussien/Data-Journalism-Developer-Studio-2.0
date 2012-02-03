b06a20feba3ac5898d1509f1cd777af4c8af126d
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-03 12:05:50 -0800

    desktop tweaks

--- Install-Scripts/configure-desktop.bash
+++ Install-Scripts/configure-desktop.bash
@@ -4,9 +4,10 @@ cat bashrc >> ~/.bashrc # define the ls conveniences
 vim ~/.bashrc
 source ~/.bashrc
 
-mkdir -p ~/.config/openbox; cp -a openbox-conf/* ~/.config/openbox/
+mkdir -p ~/.config/libfm; cp -a libfm-conf/* ~/.config/libfm/
 mkdir -p ~/.config/lxterminal; cp -a lxterminal-conf/* ~/.config/lxterminal/
 mkdir -p ~/.config/lxpanel; cp -a lxpanel-conf/* ~/.config/lxpanel/
+mkdir -p ~/.config/lxsession; cp -a lxsession-conf/* ~/.config/lxsession/
+mkdir -p ~/.config/openbox; cp -a openbox-conf/* ~/.config/openbox/
+mkdir -p ~/.config/pcmanfm; cp -a pcmanfm-conf/* ~/.config/pcmanfm/
 mkdir -p ~/.gconf; cp -a browser-conf/* ~/.gconf/
-mkdir -p ~/GNUstep; cp -a GNUstep-conf/GNUstep/* ~/GNUstep/
-mkdir -p ~/.wmakerconf; cp -a GNUstep-conf/.wmakerconf/* ~/.wmakerconf/
