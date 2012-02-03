8ed6197b314b6c63a4e6ae5299e7fdd9727c3e82
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-02 20:40:17 -0800

    VMware in, VirtualBox out

--- Install-Scripts/install-repositories.bash
+++ Install-Scripts/install-repositories.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -v
 
-sudo zypper addrepo -cf http://download.opensuse.org/repositories/Virtualization/openSUSE_12.1/Virtualization.repo # VirtualBox guest tools
+sudo zypper addrepo -cf http://download.opensuse.org/repositories/Virtualization:/VMware/openSUSE_12.1_Update/Virtualization:VMware.repo # VMware guest tools
 sudo zypper addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/perl/openSUSE_12.1/devel:languages:perl.repo # Perl libraries
 sudo zypper addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/R:/patched/openSUSE_12.1/devel:languages:R:patched.repo # R Patched
 sudo zypper addrepo -cf http://download.opensuse.org/repositories/Documentation:/Tools/openSUSE_12.1/Documentation:Tools.repo # Tesseract
