d5b3753ac1652629a7b53a903bc7c06fc3eaa041
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-02 20:38:22 -0800

    ditched mkinitrd - shouldn't be required

--- Install-Scripts/remove-guest-tools.bash
+++ Install-Scripts/remove-guest-tools.bash
@@ -1,17 +1,14 @@
 #! /bin/bash -v
 
-# tools to build from source
+# first, install tools to build from source
 export KERNEL=`uname -r|sed 's/^.*-//'`
-sudo zypper install kernel-${KERNEL}-devel
+sudo zypper install -y kernel-${KERNEL}-devel
 
 # get rid of any VMware tools
-sudo zypper remove libvmtools0 open-vm-tools open-vm-tools-gui \
+sudo zypper remove -y libvmtools0 open-vm-tools open-vm-tools-gui \
   vmware-guest-kmp-default vmware-guest-kmp-desktop vmware-guest-kmp-pae
 
 # get rid of any VirtualBox ones
-sudo zypper remove virtualbox-guest-kmp-default virtualbox-guest-kmp-desktop \
-  virtualbox-guest-kmp-pae virtualbox-guest-tools virtualbox-guest-x11
-
-# mkinitrd and reboot
-sudo /sbin/mkinitrd
-sudo /sbin/init 6
+sudo zypper remove -y virtualbox-guest-kmp-default \
+  virtualbox-guest-kmp-desktop virtualbox-guest-kmp-pae virtualbox-guest-tools \
+  virtualbox-guest-x11
