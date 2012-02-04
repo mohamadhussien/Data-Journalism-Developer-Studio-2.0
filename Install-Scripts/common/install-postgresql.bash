962f5be51bf8c27aaaba429cfd79b6aa2ca3804e
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-04 13:35:21 -0800

    use PostgreSQL repository

--- Install-Scripts/common/install-postgresql.bash
+++ Install-Scripts/common/install-postgresql.bash
@@ -4,6 +4,8 @@ TEST=`/sbin/chkconfig | grep postgresql | wc -l`
 if [ "$TEST" != "1" ]
 then
   echo "Installing PostgreSQL and friends"
+  sudo zypper addrepo -cf http://download.opensuse.org/repositories/server:/database:/postgresql/openSUSE_12.1/server:database:postgresql.repo
+  sudo zypper refresh
   sudo zypper install -y postgresql postgresql-devel postgresql-contrib \
     pgadmin3 perl-DBD-Pg
 else
