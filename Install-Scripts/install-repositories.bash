390315c946dca8229bddcae835280b095e0bfcd1
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-04 13:54:28 -0800

    PostgreSQL

--- Install-Scripts/install-repositories.bash
+++ Install-Scripts/install-repositories.bash
@@ -5,4 +5,5 @@ sudo zypper addrepo -cf http://download.opensuse.org/repositories/devel:/languag
 sudo zypper addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/R:/patched/openSUSE_12.1/devel:languages:R:patched.repo # R Patched
 sudo zypper addrepo -cf http://download.opensuse.org/repositories/Documentation:/Tools/openSUSE_12.1/Documentation:Tools.repo # Tesseract
 sudo zypper addrepo -cf http://download.opensuse.org/repositories/X11:/lxde/openSUSE_12.1/X11:lxde.repo
+sudo zypper addrepo -cf http://download.opensuse.org/repositories/server:/database:/postgresql/openSUSE_12.1/server:database:postgresql.repo
 sudo zypper update
