99eb2ce4e4e5508fa7fc4ae758df22293c73d75d
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-07 20:00:38 -0800

    Sigil ain't here no more

--- Install-Scripts/ReproducibleResearch/cleanup.bash
+++ Install-Scripts/ReproducibleResearch/cleanup.bash
@@ -3,4 +3,3 @@
 rm -f INSTALL literate-article.layout literate-book.layout \
   literate-report.layout literate-scrap.inc preferences README \
   Sweave-test-1.lyx template-vignette.lyx test.lyx *.log
-rm -fr Sigil*
