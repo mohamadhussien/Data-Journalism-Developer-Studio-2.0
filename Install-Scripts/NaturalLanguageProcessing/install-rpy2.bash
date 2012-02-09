8140d9799a481ea34d186842e444c91202a82541
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-09 11:10:06 -0800

    updated to latest RPy2

--- Install-Scripts/NaturalLanguageProcessing/install-rpy2.bash
+++ Install-Scripts/NaturalLanguageProcessing/install-rpy2.bash
@@ -2,9 +2,9 @@
 
 # RPy2 - Python / R interface
 sudo mkdir -p /usr/local/src; cd /usr/local/src
-DIR=rpy2-2.1.8
+DIR=rpy2-2.2.1
 WHAT=${DIR}.tar.gz
-WHERE=http://downloads.sourceforge.net/project/rpy/rpy2/2.1.x
+WHERE=http://downloads.sourceforge.net/project/rpy/rpy2/2.2.x
 sudo rm -fr ${WHAT} ${DIR}
 sudo wget ${WHERE}/${WHAT}
 sudo tar xf ${WHAT}
