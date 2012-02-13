90f4e7a60e90e15a85e64ad27dcdea6c57e2fe62
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-13 04:35:45 -0800

    Node now in base

--- Install-Scripts/install-base.bash
+++ Install-Scripts/install-base.bash
@@ -13,7 +13,7 @@ then
 fi
 
 # install addons
-for addon in R-libraries Rcmdr Perl-packages GoogleRefine Maqetta Sigil
+for addon in R-libraries Rcmdr Perl-packages GoogleRefine Maqetta Sigil Node
 do
   pushd ${addon}
   ./cleanup.bash
