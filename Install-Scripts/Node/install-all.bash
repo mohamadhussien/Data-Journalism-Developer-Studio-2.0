29ddc53aaae55ed99db9fceb68d90a5edaec8a14
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-13 04:34:15 -0800

    dropped "now"; make archive

--- Install-Scripts/Node/install-all.bash
+++ Install-Scripts/Node/install-all.bash
@@ -4,3 +4,17 @@ for i in node packages
 do
   sudo ./install-${i}.bash 2>&1 | tee ${i}.log
 done
+
+pushd ~
+sudo tar cf - \
+  /usr/local/lib/node \
+  /usr/local/lib/node_modules \
+  /usr/local/include/node \
+  /usr/local/share/man/man1/node.1 \
+  /usr/local/bin/node \
+  /usr/local/bin/node-waf \
+  /usr/local/bin/cake \
+  /usr/local/bin/coffee \
+  /usr/local/bin/npm \
+  | bzip2 -9c > node.tar.bz2
+popd
--- Install-Scripts/Node/install-packages.bash
+++ Install-Scripts/Node/install-packages.bash
@@ -2,9 +2,6 @@
 
 export PATH=/usr/local/bin:$PATH
 
-# now.js
-npm install now
-
 # CoffeeScript
 npm install -g coffee-script
 
