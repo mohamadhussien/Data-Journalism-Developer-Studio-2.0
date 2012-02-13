0e97d2a8a5c8a27a6ba49d557c89de86f4fd7324
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-13 03:46:00 -0800

    install as "root" because of pgfWeave script

--- Install-Scripts/ReproducibleResearch/install-all.bash
+++ Install-Scripts/ReproducibleResearch/install-all.bash
@@ -1,4 +1,4 @@
 #! /bin/bash -v
 
-./install-task-view.bash 2>&1 | tee task-view.log
+sudo ./install-task-view.bash 2>&1 | tee task-view.log
 ./install-Sweave-lyx.bash 2>&1 | tee Sweave-lyx.log
--- Install-Scripts/ReproducibleResearch/load-task-view.R
+++ Install-Scripts/ReproducibleResearch/load-task-view.R
@@ -1,2 +1,3 @@
+install.packages('ctv', repos='http://cran.cnr.berkeley.edu')
 library(ctv)
 install.views('ReproducibleResearch', repos='http://cran.cnr.berkeley.edu')
