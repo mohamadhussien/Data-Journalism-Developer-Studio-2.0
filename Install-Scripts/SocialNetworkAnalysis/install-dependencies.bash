ea31ea2773eeea5c9426d73aff69e1d1a02e2eb2
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-13 20:35:39 -0800

    Python network analysis tools

--- Install-Scripts/SocialNetworkAnalysis/install-dependencies.bash
+++ Install-Scripts/SocialNetworkAnalysis/install-dependencies.bash
@@ -1,3 +1,11 @@
 #! /bin/bash -v
 
+# Repository
+sudo zypper addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_12.1/devel:languages:python.repo
+sudo zypper refresh
+
+# Python tools for Network analysis
+sudo zypper install --no-recommends -y python-devel python-idle python-networkx
+sudo zypper install --no-recommends -y python-numpy python-matplotlib
+
 sudo zypper install --no-recommends -y graphviz graphviz-devel graphviz-gd graphviz-gnome
