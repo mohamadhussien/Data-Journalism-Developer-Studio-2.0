a1802a3016db657fd8bd86746abc07bdee9457c2
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-13 23:09:45 -0800

    no chromium-desktop-gnome, just chromium

--- Install-Scripts/BrowserDevelopment/install-dependencies.bash
+++ Install-Scripts/BrowserDevelopment/install-dependencies.bash
@@ -1,6 +1,6 @@
 #! /bin/bash -v
 
-sudo zypper install --no-recommends -y chromium-desktop-gnome vpx-tools
+sudo zypper install --no-recommends -y chromium vpx-tools
 sudo zypper install --no-recommends -y v8-devel libvpx-devel
 sudo zypper install --no-recommends -y MozillaFirefox-devel
 sudo zypper install --no-recommends -y seamonkey seamonkey-dom-inspector seamonkey-venkman
