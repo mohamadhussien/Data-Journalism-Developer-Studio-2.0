59d504578057cecb48bc5cd70208cc19bd9fd8d5
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-14 19:24:00 -0800

    missing backslash

--- Install-Scripts/SemanticDesktop/install-dependencies.bash
+++ Install-Scripts/SemanticDesktop/install-dependencies.bash
@@ -1,6 +1,6 @@
 #! /bin/bash -v
 
-sudo zypper --quiet install --no-recommends -y evolution evolution-data-server
+sudo zypper --quiet install --no-recommends -y evolution evolution-data-server \
   evolution-webcal MozillaThunderbird
 
 sudo zypper --quiet install --no-recommends -y tracker tracker-miner-evolution \
