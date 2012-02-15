e08e3adf3e71f5e8a3b65b29aebc4c24bd4e64c1
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-14 19:32:10 -0800

    naming error

--- Install-Scripts/Riak/install-dependencies.bash
+++ Install-Scripts/Riak/install-dependencies.bash
@@ -1,5 +1,5 @@
 #! /bin/bash -v
 
-sudo zypper --quiet addrepo -n -cf http://download.opensuse.org/repositories/server:/database/openSUSE_Factory databases_Factory 
+sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/server:/database/openSUSE_Factory databases_Factory 
 sudo zypper --quiet refresh
 sudo zypper --quiet install --no-recommends -y riak
