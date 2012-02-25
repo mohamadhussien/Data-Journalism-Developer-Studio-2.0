9c7735cac3fc5442941ef1e4066ba11ff68a239c
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-24 19:10:17 -0800

    back to local install

--- Install-Scripts/GoogleRefine/install-google-refine.bash
+++ Install-Scripts/GoogleRefine/install-google-refine.bash
@@ -4,22 +4,21 @@ export WHAT=google-refine-2.5-r2407.tar.gz
 export VERSION=`echo ${WHAT}|sed 's/.tar.gz//'`
 export DIR=google-refine-2.5
 
-pushd /usr/local
-sudo rm -fr ${WHAT} ${DIR}
-sudo wget ${WHERE}/${WHAT}
-sudo tar xf ${WHAT}
-sudo rm ${WHAT}
+mkdir -p ${HOME}/local
+pushd ${HOME}/local
+rm -fr ${WHAT} ${DIR}
+wget ${WHERE}/${WHAT}
+tar xf ${WHAT}
+rm ${WHAT}
 popd
 
-sudo mkdir -p /usr/local/share/applications
-sudo cp google-refine.desktop /usr/local/share/applications/
-sudo mkdir -p /usr/local/bin
-sudo cp refine /usr/local/bin/
+mkdir -p ${HOME}/.local/share/applications
+sed "s;\$HOME;${HOME};" google-refine.desktop \
+  > ${HOME}/.local/share/applications/google-refine.desktop
 
 pushd ~
-sudo tar cf - \
-  /usr/local/share/applications/google-refine.desktop \
-  /usr/local/bin/refine \
-  /usr/local/${DIR} \
+tar cf - \
+  .local/share/applications/google-refine.desktop \
+  local/${DIR} \
   | bzip2 -9c > ${VERSION}.tar.bz2
 popd
