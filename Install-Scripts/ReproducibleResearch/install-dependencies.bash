1feae15881a5afb91dc70166fb488bb00d785617
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-26 23:28:16 -0800

    just need LyX - it brings in the needed stuff

--- Install-Scripts/ReproducibleResearch/install-dependencies.bash
+++ Install-Scripts/ReproducibleResearch/install-dependencies.bash
@@ -3,7 +3,6 @@
 # sigh - need the *whole* texlive-latex for pgfSweave :-(
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_12.1/Publishing.repo # LaTeX stuff
 sudo zypper --quiet refresh
-#sudo zypper --quiet install --no-recommends -y texlive texlive-latex texlive-xetex
 
-# For Sweave / LyX
+# LyX brings in everything we need
 sudo zypper --quiet install --no-recommends -y lyx
