ee421ac71707bcdb4dcef1652bbe6913dae7aac0
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-26 23:13:29 -0800

    minimizing LaTeX dependencies

--- Install-Scripts/ReproducibleResearch/install-dependencies.bash
+++ Install-Scripts/ReproducibleResearch/install-dependencies.bash
@@ -1,8 +1,9 @@
 #! /bin/bash -v
 
 # sigh - need the *whole* texlive-latex for pgfSweave :-(
-sudo zypper --quiet install --no-recommends -y texlive texlive-latex texlive-xetex
+sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_12.1/Publishing.repo # LaTeX stuff
+sudo zypper --quiet refresh
+#sudo zypper --quiet install --no-recommends -y texlive texlive-latex texlive-xetex
 
 # For Sweave / LyX
-sudo zypper --quiet install --no-recommends -y lyx latex2html \
-  latex2html-pngicons texlive-tex4ht
+sudo zypper --quiet install --no-recommends -y lyx
