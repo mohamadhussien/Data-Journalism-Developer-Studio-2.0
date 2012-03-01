b4d958bd4d8bd73ff77521741b10b3446a4a420c
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-02-29 16:11:01 -0800

    seems to need gstreamer-0_10-devel to build now

--- Install-Scripts/Sigil/install-dependencies.bash
+++ Install-Scripts/Sigil/install-dependencies.bash
@@ -1,4 +1,4 @@
 #! /bin/bash -v
 
 # needed for Sigil
-sudo zypper --quiet install --no-recommends -y cmake libqt4-devel libQtWebKit-devel
+sudo zypper --quiet install --no-recommends -y cmake libqt4-devel libQtWebKit-devel gstreamer-0_10-devel
