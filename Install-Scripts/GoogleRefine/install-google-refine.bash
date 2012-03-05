07fbbbdf67d4a224788e8e11d3ae25fd2b75bd79
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-05 10:40:09 -0800

    refactored the version code since I can't find it automagically

--- Install-Scripts/GoogleRefine/install-google-refine.bash
+++ Install-Scripts/GoogleRefine/install-google-refine.bash
@@ -1,8 +1,8 @@
 #! /bin/bash -v
 export WHERE=http://google-refine.googlecode.com/files
-export WHAT=google-refine-2.5-r2407.tar.gz
-export VERSION=`echo ${WHAT}|sed 's/.tar.gz//'`
 export DIR=google-refine-2.5
+export VERSION=${DIR}-r2407
+export WHAT=${VERSION}.tar.gz
 
 mkdir -p ${HOME}/local
 pushd ${HOME}/local
@@ -13,7 +13,7 @@ rm ${WHAT}
 popd
 
 mkdir -p ${HOME}/.local/share/applications
-sed "s;\$HOME;${HOME};" google-refine.desktop \
+sed "s;\$BASE;${HOME}/local/${DIR};" google-refine.desktop \
   > ${HOME}/.local/share/applications/google-refine.desktop
 
 pushd ~
