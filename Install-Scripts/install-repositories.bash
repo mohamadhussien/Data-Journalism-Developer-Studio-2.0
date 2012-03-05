2c3d049c76f515631464e0469ac7dd2aa3c977d4
M. Edward (Ed) Borasky <znmeb@AlgoCompSynth.(none)>
2012-03-05 10:11:35 -0800

    getting ready for some trimming

--- Install-Scripts/install-repositories.bash
+++ Install-Scripts/install-repositories.bash
@@ -3,7 +3,7 @@
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Virtualization:/VMware/openSUSE_12.1_Update/Virtualization:VMware.repo # VMware guest tools
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/perl/openSUSE_12.1/devel:languages:perl.repo # Perl libraries
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/R:/patched/openSUSE_12.1/devel:languages:R:patched.repo # R Patched
-sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Documentation:/Tools/openSUSE_12.1/Documentation:Tools.repo # Tesseract
+sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Documentation:/Tools/openSUSE_12.1/Documentation:Tools.repo # Tesseract and Calibre
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/X11:/lxde/openSUSE_12.1/X11:lxde.repo # LXDE
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/server:/database:/postgresql/openSUSE_12.1/server:database:postgresql.repo # PostgreSQL
 sudo zypper --quiet update
