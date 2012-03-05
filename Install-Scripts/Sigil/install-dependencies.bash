3d1277db1fb9b186357f52985914bbff77d87ea3
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-05 11:17:38 -0800

    moar dependencies

--- Install-Scripts/Sigil/install-dependencies.bash
+++ Install-Scripts/Sigil/install-dependencies.bash
@@ -1,4 +1,7 @@
 #! /bin/bash -v
 
 # needed for Sigil
-sudo zypper --quiet install --no-recommends -y cmake libqt4-devel libQtWebKit-devel gstreamer-0_10-devel
+sudo zypper --quiet install --no-recommends -y cmake libqt4-devel libQtWebKit-devel gstreamer-0_10-devel boost-devel
+sudo zypper --quiet install --no-recommends -y libxerces-c-devel xerces-c 
+sudo zypper --quiet install --no-recommends -y hunspell-devel
+sudo zypper --quiet install --no-recommends -y pcre-devel
