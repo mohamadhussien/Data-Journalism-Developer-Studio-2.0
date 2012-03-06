2da4562aa1d7bd0ced168b8213160817f09efc61
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-06 14:47:33 -0800

    no-recommends!!

--- Install-Scripts/install-repositories.bash
+++ Install-Scripts/install-repositories.bash
@@ -7,5 +7,5 @@ sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Docume
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/X11:/lxde/openSUSE_12.1/X11:lxde.repo # LXDE
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/server:/database:/postgresql/openSUSE_12.1/server:database:postgresql.repo # PostgreSQL
 
-sudo zypper --quiet update
-sudo zypper --quiet dup
+sudo zypper --quiet --no-recommends update
+sudo zypper --quiet --no-recommends dist-upgrade
