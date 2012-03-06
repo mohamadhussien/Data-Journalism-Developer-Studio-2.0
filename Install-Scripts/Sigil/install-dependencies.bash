b8f7c40f6ae2bab3c8e2dec3f58952f4d298cf29
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-05 21:42:25 -0800

    pulled out some libs we shouldn't need

--- Install-Scripts/Sigil/install-dependencies.bash
+++ Install-Scripts/Sigil/install-dependencies.bash
@@ -2,6 +2,6 @@
 
 # needed for Sigil
 sudo zypper --quiet install --no-recommends -y cmake libqt4-devel libQtWebKit-devel gstreamer-0_10-devel boost-devel
-sudo zypper --quiet install --no-recommends -y libxerces-c-devel xerces-c 
+sudo zypper --quiet install --no-recommends -y libxerces-c-devel
 sudo zypper --quiet install --no-recommends -y hunspell-devel
 sudo zypper --quiet install --no-recommends -y pcre-devel
