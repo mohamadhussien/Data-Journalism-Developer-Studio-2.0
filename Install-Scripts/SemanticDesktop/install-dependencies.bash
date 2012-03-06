1e0d7131a2f808e63f6bc1e893b69912a53ec6e8
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-06 14:41:55 -0800

    added Virtuoso

--- Install-Scripts/SemanticDesktop/install-dependencies.bash
+++ Install-Scripts/SemanticDesktop/install-dependencies.bash
@@ -1,8 +1,10 @@
 #! /bin/bash -v
 
 sudo zypper --quiet install --no-recommends -y evolution evolution-data-server \
-  evolution-webcal MozillaThunderbird
+  evolution-webcal MozillaThunderbird evolution-plugin-rss
 
 sudo zypper --quiet install --no-recommends -y tracker tracker-miner-evolution \
   tracker-miner-files tracker-miner-flickr tracker-miner-rss tracker-gui \
-  tracker-miner-firefox tracker-miner-thunderbird
+  tracker-miner-firefox tracker-miner-thunderbird tracker-devel tracker-lang
+
+sudo zypper --quiet install --no-recommends -y virtuoso-drivers virtuoso-server
