c594ee11e5400883b2dd14cceb4af4f5f7aa3b79
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-05 21:49:50 -0800

    Django now in base!

--- Install-Scripts/Django/install-dependencies.bash
+++ Install-Scripts/Django/install-dependencies.bash
@@ -1,7 +1,3 @@
 #! /bin/bash -v
 
-# Repository
-sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_12.1/devel:languages:python.repo
-sudo zypper --quiet refresh
-
 sudo zypper --quiet install --no-recommends -y python-django
