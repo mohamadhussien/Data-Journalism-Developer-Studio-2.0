d89d1b658b293d46e2d6750824a0e9186f0d76e4
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-06 14:04:50 -0800

    python-devel and python-idle added

--- Install-Scripts/common/install-python-repositories.bash
+++ Install-Scripts/common/install-python-repositories.bash
@@ -3,3 +3,5 @@
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_12.1/devel:languages:python.repo
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/devel:/libraries:/c_c++/openSUSE_12.1/devel:libraries:c_c++.repo # Python needs this
 sudo zypper refresh
+
+sudo zypper --quiet install --no-recommends -y python-devel python-idle
