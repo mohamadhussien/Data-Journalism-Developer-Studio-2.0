6b8819f856be33134d4dfa57fcb6e56e787e55f6
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-06 11:43:19 -0800

    quiet, not quite

--- Install-Scripts/Sigil/install-dependencies.bash
+++ Install-Scripts/Sigil/install-dependencies.bash
@@ -2,10 +2,10 @@
 
 # needed for Sigil
 sudo zypper --quiet install --no-recommends -y cmake
-sudo zypper --quite install --no-recommends -y libqt4-devel
-sudo zypper --quite install --no-recommends -y libQtWebKit-devel
-sudo zypper --quite install --no-recommends -y gstreamer-0_10-devel
-sudo zypper --quite install --no-recommends -y boost-devel
+sudo zypper --quiet install --no-recommends -y libqt4-devel
+sudo zypper --quiet install --no-recommends -y libQtWebKit-devel
+sudo zypper --quiet install --no-recommends -y gstreamer-0_10-devel
+sudo zypper --quiet install --no-recommends -y boost-devel
 sudo zypper --quiet install --no-recommends -y libxerces-c-devel
 sudo zypper --quiet install --no-recommends -y hunspell-devel
 sudo zypper --quiet install --no-recommends -y pcre-devel
