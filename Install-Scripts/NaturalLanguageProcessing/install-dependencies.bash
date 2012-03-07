dcf7caeeda223fd71b01ceb4c9cfc445cb46ba7a
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-06 17:11:32 -0800

    gsl-devel

--- Install-Scripts/NaturalLanguageProcessing/install-dependencies.bash
+++ Install-Scripts/NaturalLanguageProcessing/install-dependencies.bash
@@ -3,4 +3,4 @@
 ../common/install-python-repositories.bash
 sudo zypper --quiet install --no-recommends -y ant # for Mallet
 sudo zypper --quiet install --no-recommends -y python-nltk
-sudo zypper --quiet install --no-recommends -y wordnet wordnet-devel
+sudo zypper --quiet install --no-recommends -y wordnet wordnet-devel gsl-devel
