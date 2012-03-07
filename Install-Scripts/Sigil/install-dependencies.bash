5e9c92af8f6e48c541ce01a94b6dd9353103192f
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-06 23:33:42 -0800

    don't need xerces-c-devel - too old anyhow

--- Install-Scripts/Sigil/install-dependencies.bash
+++ Install-Scripts/Sigil/install-dependencies.bash
@@ -6,6 +6,5 @@ sudo zypper --quiet install --no-recommends -y libqt4-devel
 sudo zypper --quiet install --no-recommends -y libQtWebKit-devel
 sudo zypper --quiet install --no-recommends -y gstreamer-0_10-devel
 sudo zypper --quiet install --no-recommends -y boost-devel
-sudo zypper --quiet install --no-recommends -y libxerces-c-devel
 sudo zypper --quiet install --no-recommends -y hunspell-devel
 sudo zypper --quiet install --no-recommends -y pcre-devel
