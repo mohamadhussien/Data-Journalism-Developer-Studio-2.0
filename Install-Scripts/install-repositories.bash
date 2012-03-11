98d0602aacb2b228f65f4049dc7c56f1095984d0
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-10 17:40:05 -0800

    Multimedia repos crash Calibre :-(

--- Install-Scripts/install-repositories.bash
+++ Install-Scripts/install-repositories.bash
@@ -6,8 +6,5 @@ sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/devel:
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Documentation:/Tools/openSUSE_12.1/Documentation:Tools.repo # Tesseract and Calibre
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/X11:/lxde/openSUSE_12.1/X11:lxde.repo # LXDE
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/server:/database:/postgresql/openSUSE_12.1/server:database:postgresql.repo # PostgreSQL
-sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/multimedia:/apps/openSUSE_12.1/multimedia:apps.repo
-sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/multimedia:/libs/openSUSE_12.1/multimedia:libs.repo
 
 sudo zypper --quiet --no-recommends update
-sudo zypper --quiet --no-recommends dist-upgrade
