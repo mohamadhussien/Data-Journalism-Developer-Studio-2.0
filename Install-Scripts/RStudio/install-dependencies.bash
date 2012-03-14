33cdd213f884df7d300e8d9381abd6792eb5e1be
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-14 10:43:52 -0700

    added TeXmacs, which needs Factory Publishing repo

--- Install-Scripts/RStudio/install-dependencies.bash
+++ Install-Scripts/RStudio/install-dependencies.bash
@@ -1,6 +1,6 @@
 #! /bin/bash -v
 
-sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_12.1/Publishing.repo # LaTeX stuff
+sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_Factory/Publishing.repo
 sudo zypper --quiet refresh
 sudo zypper --quiet install --no-recommends -y texinfo
 sudo zypper --quiet install --no-recommends -y texlive-bin
--- Install-Scripts/ReproducibleResearch/install-dependencies.bash
+++ Install-Scripts/ReproducibleResearch/install-dependencies.bash
@@ -1,8 +1,9 @@
 #! /bin/bash -v
 
 # sigh - need the *whole* texlive-latex for pgfSweave :-(
-sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_12.1/Publishing.repo # LaTeX stuff
+sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_Factory/Publishing.repo
 sudo zypper --quiet refresh
 
 # LyX brings in everything we need
 sudo zypper --quiet install --no-recommends -y lyx
+sudo zypper --quiet install --no-recommends -y TeXmacs TeXmacs-examples
