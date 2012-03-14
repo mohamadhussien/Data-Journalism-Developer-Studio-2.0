46f630e9656484dccf8c76572d29df5d314e6769
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-14 14:40:17 -0700

    added more LaTeX tools - pretty much everything

--- Install-Scripts/ReproducibleResearch/install-dependencies.bash
+++ Install-Scripts/ReproducibleResearch/install-dependencies.bash
@@ -4,6 +4,28 @@
 sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_Factory/Publishing.repo
 sudo zypper --quiet refresh
 
-# LyX brings in everything we need
 sudo zypper --quiet install --no-recommends -y lyx
 sudo zypper --quiet install --no-recommends -y TeXmacs TeXmacs-examples
+sudo zypper --quiet install --no-recommends -y TeX-Guy
+sudo zypper --quiet install --no-recommends -y dblatex
+sudo zypper --quiet install --no-recommends -y html2text
+sudo zypper --quiet install --no-recommends -y latex-driver
+sudo zypper --quiet install --no-recommends -y latex2html
+sudo zypper --quiet install --no-recommends -y latex2html
+sudo zypper --quiet install --no-recommends -y texcad
+sudo zypper --quiet install --no-recommends -y texinfo
+sudo zypper --quiet install --no-recommends -y texlive-arab
+sudo zypper --quiet install --no-recommends -y texlive-cjk
+sudo zypper --quiet install --no-recommends -y texlive-cjk-latex-extras
+sudo zypper --quiet install --no-recommends -y texlive-fonts-extra
+sudo zypper --quiet install --no-recommends -y texlive-jadetex
+sudo zypper --quiet install --no-recommends -y texlive-metapost
+sudo zypper --quiet install --no-recommends -y texlive-musictex
+sudo zypper --quiet install --no-recommends -y texlive-omega
+sudo zypper --quiet install --no-recommends -y texlive-ppower4
+sudo zypper --quiet install --no-recommends -y texlive-tex4ht
+sudo zypper --quiet install --no-recommends -y texlive-tools
+sudo zypper --quiet install --no-recommends -y texlive-xmltex
+sudo zypper --quiet install --no-recommends -y texmaker
+sudo zypper --quiet install --no-recommends -y texworks
+sudo zypper --quiet install --no-recommends -y vim-plugin-latex
