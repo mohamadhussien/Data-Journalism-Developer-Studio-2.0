d207c9288c9dff24d53607aa3be96d56fd356268
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-15 01:38:51 -0700

    fixed version bugs

--- Install-Scripts/ReproducibleResearch/install-dependencies.bash
+++ Install-Scripts/ReproducibleResearch/install-dependencies.bash
@@ -1,12 +1,14 @@
 #! /bin/bash -v
 
 # sigh - need the *whole* texlive-latex for pgfSweave :-(
-sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_Factory/Publishing.repo
+sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_Factory/ 'Publishing Factory'
+sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_12.1/ 'Publishing 12.1'
 sudo zypper --quiet refresh
 
-sudo zypper --quiet install --no-recommends -y lyx
+sudo zypper --quiet install --no-recommends -y lyx-2.0.3-55.1
+sudo zypper --quiet install --no-recommends -y xindy xindy-rules
 sudo zypper --quiet install --no-recommends -y TeXmacs TeXmacs-examples
-sudo zypper --quiet install --no-recommends -y TeX-Guy
+sudo zypper --quiet install --no-recommends -y TeX-Guy-1.3.1-205.1.2
 sudo zypper --quiet install --no-recommends -y dblatex
 sudo zypper --quiet install --no-recommends -y html2text
 sudo zypper --quiet install --no-recommends -y latex-driver
