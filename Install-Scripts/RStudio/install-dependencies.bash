a528e8fe2fd4351d85566ae8d9d8a96eb7b82303
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-15 01:40:55 -0700

    sync repos with Reproducible Results

--- Install-Scripts/RStudio/install-dependencies.bash
+++ Install-Scripts/RStudio/install-dependencies.bash
@@ -1,7 +1,9 @@
 #! /bin/bash -v
 
-sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_Factory/Publishing.repo
+sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_Factory/ 'Publishing Factory'
+sudo zypper --quiet addrepo -cf http://download.opensuse.org/repositories/Publishing/openSUSE_12.1/ 'Publishing 12.1'
 sudo zypper --quiet refresh
+
 sudo zypper --quiet install --no-recommends -y texinfo
 sudo zypper --quiet install --no-recommends -y texlive-bin
 sudo zypper --quiet install --no-recommends -y texlive-bin-latex
