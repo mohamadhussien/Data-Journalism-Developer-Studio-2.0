aeb3c73cc494792818ce45a62bb4cec5e22a0e3f
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2012-03-15 23:10:51 -0700

    5.11

--- Install-Scripts/AlgoCompSynth/install-impro-visor.bash
+++ Install-Scripts/AlgoCompSynth/install-impro-visor.bash
@@ -2,8 +2,8 @@
 
 # download latest version
 rm *unix*.sh
-export WHERE="http://downloads.sourceforge.net/project/impro-visor/Impro-Visor%205.10%20Release"
-export WHAT="Impro-Visor_unix_5_10.sh"
+export WHERE="http://downloads.sourceforge.net/project/impro-visor/Impro-Visor%205.11%20Release"
+export WHAT="Impro-Visor_unix_5_11.sh"
 wget $WHERE/$WHAT
 
 # install
